bb824e65919405e73fa93f8a42061eddea0b3fb6
Saicharan1425 <nsaic1425@gmail.com>
2026-07-19 18:59:38 +0530

    Tic-Tac_19

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react"
 
+export default function App(){
 
-import './App.css'
-import { StopWatch } from './StopWatch'
+  const [square,setSqr]= useState(Array(9).fill(null))
 
+  function onsqrClick(i){
+    let ayoo = [...square]
 
-function App() {
+    ayoo[i]='X'
+   
+    setSqr(ayoo)
+  }
 
-  
-
-
-  return (
-    
-      <StopWatch></StopWatch>
-  
+  return(
+    <>
+    <div className="board-row">
+    <Square value={square[0]} sqrClick={()=>onsqrClick(0)}/>
+    <Square value={square[1]} sqrClick={()=>onsqrClick(1)}/>
+    <Square value={square[2]} sqrClick={()=>onsqrClick(2)}/>
+    </div>
+    <div className="board-row">
+    <Square value={square[3]} sqrClick={()=>onsqrClick(3)}/>
+    <Square value={square[4]} sqrClick={()=>onsqrClick(4)}/>
+    <Square value={square[5]} sqrClick={()=>onsqrClick(5)}/>
+    </div>
+    <div className="board-row">
+    <Square value={square[6]} sqrClick={()=>onsqrClick(6)}/>
+    <Square value={square[7]} sqrClick={()=>onsqrClick(7)}/>
+    <Square value={square[8]} sqrClick={()=>onsqrClick(8)}/>
+    </div>
+   
+    </>
   )
 }
 
-export default App
+function Square({value,sqrClick}){
+
+
+
+  return <button className="square" onClick={sqrClick} >{value}</button>
+}
+
